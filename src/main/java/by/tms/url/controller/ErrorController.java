42c0dbecb8fd385275a07d72187a11c58798bada
ArtyomKhodas <a.Khodas1601@gmail.com>
2023-07-11 19:15:25 +0300

    add ErrorController.java

--- src/main/java/by/tms/url/controller/ErrorController.java
+++ src/main/java/by/tms/url/controller/ErrorController.java
@@ -1,9 +1,29 @@
 package by.tms.url.controller;
 
+import by.tms.url.exception.BusinessException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 @ControllerAdvice
 @Slf4j
 public class ErrorController {
+    @ExceptionHandler(BusinessException.class)
+    public ModelAndView onBusinessException(BusinessException e) {
+        log.warn("Business error: {}", e.toString());
+        return new ModelAndView("error-page", Map.of(
+                "errorMessage", e.getMessage()
+        ));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView onException(Exception e) {
+        log.error("Unknown error:", e);
+        return new ModelAndView("error-page", Map.of(
+                "errorMessage", "Unknown error"
+        ));
+    }
 }
